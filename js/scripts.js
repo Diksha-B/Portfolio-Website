9d6f64fa169a63c4de7fb21fb0d182207c0269a2
Diksha-B <67030726+Diksha-B@users.noreply.github.com>
2023-05-11 13:52:25 -0400

    add Javascript &jsquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log ("Hey, you. Welcome to my portfolio!");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+    } else {
+    x.className = 'navtoggle';
+    }
+}
